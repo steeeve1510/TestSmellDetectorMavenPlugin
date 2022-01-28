2bd86ed8ab9e2d0de53ddc0608909a75a98c865a
Stefan Heigl <stefan.heigl90@gmail.com>
2022-01-28 19:35:57 +0100

    initial commit

---
+++ build-and-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mvn clean install && mvn com.heigl:test-smell-detector-maven-plugin:1.0-SNAPSHOT:detect
---
+++ src/main/java/com/heigl/testsmelldetector/maven/TestSmellDetectorMojo.java
@@ -0,0 +1,31 @@
+package com.heigl.testsmelldetector.maven;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
+
+@Mojo(name = "detect", defaultPhase = LifecyclePhase.COMPILE)
+public class TestSmellDetectorMojo extends AbstractMojo {
+
+    @Parameter(property = "scope")
+    String scope;
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        List<Dependency> dependencies = project.getDependencies();
+        long numDependencies = dependencies.stream()
+                .filter(d -> (scope == null || scope.isEmpty()) || scope.equals(d.getScope()))
+                .count();
+        getLog().info("Number of dependencies: " + numDependencies);
+    }
+}
