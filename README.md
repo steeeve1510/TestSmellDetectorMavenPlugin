8cb9534cdcd85a45f791bf1ae4ec94e987e2e72f
Stefan Heigl <stefan.heigl90@gmail.com>
2022-01-31 20:23:38 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # TestSmellDetectorMavenPlugin
 
 https://www.baeldung.com/maven-plugin
+https://maven.apache.org/guides/plugin/guide-java-plugin-development.html
--- src/main/java/com/heigl/testsmelldetector/maven/TestSmellDetectorMojo.java
+++ src/main/java/com/heigl/testsmelldetector/maven/TestSmellDetectorMojo.java
@@ -1,11 +1,10 @@
 package com.heigl.testsmelldetector.maven;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import testsmell.TestFile;
 import testsmell.TestSmellDetector;
@@ -13,7 +12,10 @@ import thresholds.DefaultThresholds;
 
 import java.util.List;
 
-@Mojo(name = "detect", defaultPhase = LifecyclePhase.COMPILE)
+@Mojo(
+        name = "detect",
+        defaultPhase = LifecyclePhase.COMPILE
+)
 public class TestSmellDetectorMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
@@ -25,7 +27,7 @@ public class TestSmellDetectorMojo extends AbstractMojo {
     private final TestSmellWriter testSmellWriter = new TestSmellWriter(testSmellDetector, getLog());
 
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() {
         List<String> testCompileSourceRoots = project.getTestCompileSourceRoots();
         String appName = project.getName();
         String outputFile = project.getBuild().getDirectory() + "/test-smells.csv";
